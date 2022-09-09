6e367bbdbff73076acd99defa90f630ebd65d2c3
TheEngrNest <https://www.automation-concepts.co.in/>
2022-09-09 21:25:08 +0530

    pytest-tutorial-5 code

---
+++ calculator_app/calculator.py
@@ -0,0 +1,17 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b:
+        return a / b
+    else:
+        return "Can't divide by 0"
