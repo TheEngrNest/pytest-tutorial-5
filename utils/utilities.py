33224c0b443588eca56124af5a1f3e7972b69700
TheEngrNest <https://www.automation-concepts.co.in/>
2022-09-09 22:15:15 +0530

    final commit

--- utils/utilities.py
+++ utils/utilities.py
@@ -1,2 +1,13 @@
 import openpyxl
 
+
+def get_testdata(sheet_name, testdata_file_name='test_data/calculator_test_data.xlsx'):
+    wb_obj = openpyxl.load_workbook(testdata_file_name)
+    data_sheet = wb_obj[sheet_name]
+    max_rw = data_sheet.max_row
+    max_cl = data_sheet.max_column
+    test_data = list()
+    for row in data_sheet.iter_rows(min_row=2, max_col=max_cl, max_row=max_rw, values_only=True):
+        test_data.append(row)
+        print(row)
+    return test_data
