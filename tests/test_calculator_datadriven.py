a6707b0a81442c08a26214441935ad7f4a91b294
TheEngrNest <https://www.automation-concepts.co.in/>
2022-09-09 21:36:58 +0530

    added empty test_data folder

---
+++ tests/test_calculator_datadriven.py
@@ -0,0 +1,26 @@
+import pytest
+
+# Contents of tests/test_calculator_datadriven.py
+# -----------------------------------------------------
+
+from calculator_app.calculator import add, subtract, multiply, divide
+
+
+# Unit test function to test the add function
+def test_add():
+    pass
+
+
+# Unit test function to test the subtract function
+def test_subtract():
+    pass
+
+
+# Unit test function to test the multiply function
+def test_multiply():
+    pass
+
+
+# Unit test function to test the divide function
+def test_divide():
+    pass
